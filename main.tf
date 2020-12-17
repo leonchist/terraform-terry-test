218e29aa26380be4433cb2c6377284883d61bf9d
leonchrist <am000hw@gmail.com>
2020-12-16 16:46:25 -0800

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "dearie-terraform"
+    key    = "terry-test/dev"
+    region = "us-east-2"
+  }
+}
+#
+resource "aws_sqs_queue" "terraform_queue" {
+  name                      = "terry-test"
+  delay_seconds             = 90
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "Development"
+  }
+}
