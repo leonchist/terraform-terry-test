a94344442003b7a7ee77bd2f7cf86cae160f61a8
leonchrist <am000hw@gmail.com>
2020-12-17 09:42:24 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
-  name                      = "terry-test"
-  delay_seconds             = 90
-  max_message_size          = 2048
-  message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+# resource "aws_sqs_queue" "terraform_queue" {
+#   name                      = "terry-test"
+#   delay_seconds             = 90
+#   max_message_size          = 2048
+#   message_retention_seconds = 86400
+#   receive_wait_time_seconds = 10
 
-  tags = {
-    Environment = "Development"
-  }
-}
+#   tags = {
+#     Environment = "Development"
+#   }
+# }
